3b30ca2eef652a67c72bb4234bbe92f9c2378737
Ujwal <GBTC440016UR@MC.MONTGOMERYCOLLEGE.EDU>
2018-05-31 16:10:05 -0400

    test yourself solution

---
+++ src/com/company/YoutSelf.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class YoutSelf {
+
+    public static String name;
+    public static int age;
+
+    public static void main(String[] args) {
+	name = "Ujwal AC";
+	age = 29;
+	System.out.println("Name: "+ name +"\n Age: "+ age);
+
+    }
+}
